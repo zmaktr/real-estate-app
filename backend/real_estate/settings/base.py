ce678d83ab72f83659d0b07775ad639ca29b3d01
Zaeem Akhtar <zmaktr@gmail.com>
2022-12-15 18:01:07 +0500

    configured logging in settings.py

--- backend/real_estate/settings/base.py
+++ backend/real_estate/settings/base.py
@@ -123,10 +123,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
+# internal CSS, JS, fonts, images etc
 STATIC_URL = "staticfiles/"
 STATIC_ROOT = BASE_DIR / "staticfiles"
 STATICFILES_DIR = []
 
+# user uploaded files
 MEDIA_URL = "mediafiles/"
 MEDIA_ROOT = BASE_DIR / "mediafiles"
 
@@ -134,3 +136,44 @@ MEDIA_ROOT = BASE_DIR / "mediafiles"
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+# logging configuration 
+import logging
+import logging.config
+from django.utils.log import DEFAULT_LOGGING
+
+logger = logging.getLogger(__name__)
+
+LOG_LEVEL = "INFO"
+
+logging.config.dictConfig({
+    "version" : 1,
+    "disable_existing_loggers": False,
+    "formatters": {
+        "console":{
+            "format":"%(asctime)s %(name)-12s %(levelname)-8s %(message)s",
+        },
+        "file":{"format":"%(asctime)s %(name)-12s %(levelname)-8s %(message)s"},
+        "django.server": DEFAULT_LOGGING["formatters"]["django.server"],
+    },
+    "handlers":{
+        "console":{
+            "class": "logging.StreamHandler",
+            "formatter":"console",
+        },
+        "file":{
+            "level":"INFO",
+            "class":"logging.FileHandler",
+            "formatter":"file",
+            "filename":"logs/real_estate.log"
+        },
+        "django.server": DEFAULT_LOGGING["handlers"]["django.server"],
+    },
+    "loggers": {
+        "":{"level":"INFO", "handlers": ["console","file"],"propagate":False},
+        "apps":{
+            "level":"INFO", "handlers":["console"],"propagate":False
+        },
+        "django.server": DEFAULT_LOGGING["loggers"]["django.server"],
+    }
+})
