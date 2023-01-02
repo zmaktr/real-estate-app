1983126b19c292a8123f55b55cb55ff6397a2ca2
Zaeem Akhtar <zmaktr@gmail.com>
2023-01-02 20:07:40 +0500

    User model defined by extending AbstractBaseUser

--- backend/real_estate/settings/base.py
+++ backend/real_estate/settings/base.py
@@ -137,6 +137,8 @@ MEDIA_ROOT = BASE_DIR / "mediafiles"
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
+AUTH_USER_MODEL = 'users.User'
+
 # logging configuration 
 import logging
 import logging.config
