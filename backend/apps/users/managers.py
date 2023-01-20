e02a83e7afdc3ec2698523b22f89dd00dd20c403
Zaeem Akhtar <zmaktr@gmail.com>
2023-01-21 03:02:09 +0500

    timestamp abstract model and profile model added

--- backend/apps/users/managers.py
+++ backend/apps/users/managers.py
@@ -32,7 +32,7 @@ class CustomUserManager(BaseUserManager):
 
         return user
     
-    def create_supersuer(self, username, first_name, last_name, email, password, **extra_fields):
+    def create_superuser(self, username, first_name, last_name, email, password, **extra_fields):
         extra_fields.setdefault("is_staff", True)
         extra_fields.setdefault("is_superuser", True)
         extra_fields.setdefault("is_active", True)
