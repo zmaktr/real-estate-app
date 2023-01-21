aa21902c6d893c634b4abd3b4041035582ef3307
Zaeem Akhtar <zmaktr@gmail.com>
2023-01-21 21:19:33 +0500

    user model and AbstractBaseUser

--- backend/apps/users/models.py
+++ backend/apps/users/models.py
@@ -4,13 +4,14 @@ from django.db import models
 from django.utils import timezone
 from django.utils.translation import gettext_lazy as _
 from django.db import models
+from . managers import CustomUserManager
 
 class User(AbstractBaseUser, PermissionsMixin):
     pkid = models.BigAutoField(primary_key=True, editable=False)
     id = models.UUIDField(default=uuid.uuid4, editable=False, unique=True)
     username = models.CharField(verbose_name=_('Username'), max_length=255, unique=True)
-    first_name = models.CharField(verbose_name=_('First Name', max_length=50))
-    last_name = models.CharField(verbose_name=_('Last Name', max_legth=50))
+    first_name = models.CharField(verbose_name=_('First Name'), max_length=50)
+    last_name = models.CharField(verbose_name=_('Last Name'), max_length=50)
     email = models.EmailField(verbose_name=('Email Address'), unique=True)
     is_staff = models.BooleanField(default=False)
     is_active = models.BooleanField(default=True)
