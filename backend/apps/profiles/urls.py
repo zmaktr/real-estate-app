5c12850ae7fd20a7f1e6a90b7789b36efadfe8f1
Zaeem Akhtar <zmaktr@gmail.com>
2023-04-06 00:52:03 +0500

    profile urls

--- backend/apps/profiles/urls.py
+++ backend/apps/profiles/urls.py
@@ -1,10 +1,11 @@
-from django.urls import path
+from django.urls import path, include
+
 from . views import AgentListAPIView, GetProfileAPIView, TopAgentListAPIView, UpdateProfileAPIView
 
 
-urlpatterns = [
-    path('me/', GetProfileAPIView.as_view(), name='get_profile' ),
-    path('update/<str:username>/', UpdateProfileAPIView.as_view(), name='update_profile'),
-    path('agents/all/', AgentListAPIView.as_view(), name='all-agents'),
-    path('top-agents/all', TopAgentListAPIView.as_view(), name='top-agents') 
+urls_patterns = [
+    path('me/',                        GetProfileAPIView.as_view(),       name='get_profile'),
+    path('update/<str:username>/',     UpdateProfileAPIView.as_view(),    name='update_profile'),
+    path('agents/all/',                AgentListAPIView.as_view(),        name='all-agents'),
+    path('top-agents/all/',            TopAgentListAPIView.as_view(),     name='top-agents')
 ]
